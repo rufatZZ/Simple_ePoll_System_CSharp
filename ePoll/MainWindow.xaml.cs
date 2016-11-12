d91930e02a860fb2bf2d6d145016e9340e8beb2f
Rufat Zeynalli <rufat.zeinalli@gmail.com>
2016-11-12 06:33:43 -0800

    Simple ePoll system

---
+++ ePoll/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ePoll
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            //var Age = DateTime.Now.Year.ToString() - dateHop.Value.Years.ToString();
+            //else if (Age < 18)
+            //{
+            //    MessageBox.Show("Invalid Age");
+            //}
+
+            if (string.IsNullOrEmpty(textBox.Text))
+            {
+                MessageBox.Show("Please enter your name !");
+            }
+            else if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("Please enter your surname !");
+            }
+            else
+            {
+                ePoll_Select secondWindow = new ePoll_Select();
+                secondWindow.ShowDialog();
+                Application.Current.Shutdown();
+
+            }
+            
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+    }
+}
---
+++ ePoll/ePoll_Select.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ePoll
+{
+    /// <summary>
+    /// Interaction logic for ePoll_Select.xaml
+    /// </summary>
+    public partial class ePoll_Select : Window
+    {
+        public ePoll_Select()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton_Checked(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (radioButtonDark.IsChecked == true)
+            {
+                MessageBox.Show("You vote for Dark Empire. You`re Darkside. Thanks!");
+                Application.Current.Shutdown();
+            }
+            else if (radioButtonLight.IsChecked == true)
+            {
+                MessageBox.Show("You vote for Jedis. You`re Lightside. Thanks!");
+                Application.Current.Shutdown();
+            }
+            else
+            {
+                MessageBox.Show("Please Vote or Get out here !");
+            }
+        }
+    }
+}
